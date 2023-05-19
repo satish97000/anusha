6e8dfa931bde8108eda1cc29c07afbf4436fb83f
satish <satish@gmail.com>
2023-05-19 12:57:30 +0530

    wordpress executuive file created

---
+++ wordpress.sh
@@ -0,0 +1,15 @@
+# this file is created for http installation
+# version : .0
+# permissions : User , Admin
+#
+echo "installing httpd services "
+yum install httpd
+
+echo "instaling java"
+yum install java 11
+
+echo "installing maven"
+
+yum install mvn
+
+echo "installation is completed"
